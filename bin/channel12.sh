3d80e5c3a17372a090fb45b2691f0f8e28a52786
Joakim <joakim.snokvist@gmail.com>
2025-06-28 11:36:00 +0200

    Update channel12.sh

--- bin/channel12.sh
+++ bin/channel12.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 if [ "$1" -ge 980 ] && [ "$1" -le 1999 ]; then
-    echo "exec gs_toggle_recording" | nc 10.5.0.1 9500
-    tx_manager.sh set_tx_power 4
-
-elif [ "$1" -ge 1200 ] && [ "$1" -le 1499 ]; then
     auto_bitrate.sh 3400
     wfb_tx_cmd 8000 set_fec -k 3 -n 4
     tx_manager.sh set_tx_power 4 
 
-elif [ "$1" -ge 1500 ] && [ "$1" -le 2020 ]; then
-    auto_bitrate.sh 9800
+elif [ "$1" -ge 1200 ] && [ "$1" -le 1499 ]; then
+    auto_bitrate.sh 7200
     wfb_tx_cmd 8000 set_fec -k 7 -n 10
+    tx_manager.sh set_tx_power 4 
+
+elif [ "$1" -ge 1500 ] && [ "$1" -le 2020 ]; then
+    auto_bitrate.sh 14000
+    wfb_tx_cmd 8000 set_fec -k 8 -n 12
     tx_manager.sh set_tx_power 4
 fi
 
